8aebe5e773851c7b5453df79391477e0bc4bf814
Maksim <kudlik0408@gmail.com>
2018-09-28 22:26:21 +0300

    mythurd

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ button.onclick = function ( event ) {
               document.createElement ("div")
           )
           var radius = Math.random () * 300
+          var colours = ["red","yellow","white","pink","blue","black"]
+          var colour = colours[Math.floor ( Math.random () * colours.length )]
           var x = Math.random () * window.innerWidth
           var y = Math.random () * window.innerHeight
           ring.style = `
               width:${radius}px;
               height:${radius}px;
               border-radius:50%;
-              border:solid 1px red;
+              border:solid 1px ${colour};
               position:fixed;
               top:${y}px;
               left:${x}px;
